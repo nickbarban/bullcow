c4f59985a480c8eb49aef36b0cfacc900fec5b8b
nbarban <nbarban@millhouse.com>
2018-12-08 11:58:52 +0200

    init commit

---
+++ src/main/java/com/vaadinboot/bullcow/BullcowApplication.java
@@ -0,0 +1,12 @@
+package com.vaadinboot.bullcow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullcowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullcowApplication.class, args);
+    }
+}
